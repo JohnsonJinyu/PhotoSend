d24b6d227cdf80bec5baaa8e6f1610ac5213b1f0
Pengcheng-16GT <zuopengcheng00@gmail.com>
2025-11-05 11:44:59 +0800

    native C++ adjust 1.2

--- entry/src/main/cpp/Camera/camera_device.h
+++ entry/src/main/cpp/Camera/camera_device.h
@@ -1,6 +1,6 @@
 //
 // Created on 2025/11/4.
-// 相机设备管理（枚举设备、获取设备信息）
+// 专注于 WiFi（PTP/IP）相机的发现、连接、断开逻辑，维护设备连接状态。
 // Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
 // please include "napi/native_api.h".
 
