d39c2906033a57ee993796d8348a3fcda252340c
Pengcheng-16GT <zuopengcheng00@gmail.com>
2025-11-05 18:56:10 +0800

    native C++ adjust 1.0.1

--- entry/src/main/cpp/Camera/camera_device.h
+++ entry/src/main/cpp/Camera/camera_device.h
@@ -3,7 +3,7 @@
 // 相机设备管理（枚举设备、获取设备信息）
 // Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
 // please include "napi/native_api.h".
-
+// git recover test 18:55
 #ifndef PHOTOSEND_CAMERA_DEVICE_H
 #define PHOTOSEND_CAMERA_DEVICE_H
 
